4e062edbc324d3a097c66ec7196f4f1ce5ea0a9b
Zoli <t3tetete@gmail.com>
2023-02-22 17:06:26 +0100

    modified:   app/Views/Header.php

--- app/Views/Header.php
+++ app/Views/Header.php
@@ -1,12 +1,3 @@
 <nav class="navbar navbar-expand-1g navbar-dark bg-dark">
-    <a class="navbar-brand" href="/">My Photos</a>
-
-    <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
-        </button>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="/logout">Logout</a>
-            <a class="dropdown-item" href="/profile">Profile</a>
-        </div>
-    </div>
+    <a class="navbar-brand" href="/">TESZT</a>
 </nav>
